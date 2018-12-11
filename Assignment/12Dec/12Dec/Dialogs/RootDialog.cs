52af329c878f7f32ca73ae4c95ef71cb6cc834a9
shahenashaik11 <44662746+shahenashaik11@users.noreply.github.com>
2018-12-11 09:36:59 -0500

    11dec

---
+++ Assignment/12Dec/12Dec/Dialogs/RootDialog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace _12Dec.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            //if (activity.Text.Contains("Csharp")||activity.Text.Contains("C#"))
+            //{
+            //    await context.PostAsync($"Are You looking for Csharp?");
+            //}
+            //else if (activity.Text.Contains("AI")||activity.Text.Contains("Artificial Intelligence"))
+            //{
+            //    await context.PostAsync($"Are You looking for AI?");
+            //}
+            //else if(activity.Text.Contains("BOT") || activity.Text.Contains("CHAT BOT"))
+            //{
+            //    await context.PostAsync($"Are You looking for BOT?");
+            //}
+            //else
+            //{
+            //    await context.PostAsync($"Course not found!");
+            //}
+            
+
+            switch (activity.Text)
+            {
+                case "C#":
+                    await context.PostAsync($"Are you looking for C#?");
+                    break;
+                case "CSharp":
+                    await context.PostAsync($"Are you looking for C#?");
+                    break;
+                case "AI":
+                    await context.PostAsync($"Are you looking for AI?");
+                    break;
+                case "ArtificialIntelligence":
+                    await context.PostAsync($"Are you looking for AI?");
+                    break;
+                default:
+                    await context.PostAsync($"Course not found!");
+                    break;
+            }
+
+
+            // Calculate something for us to return
+            // int length = (activity.Text ?? string.Empty).Length;
+
+            // Return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
